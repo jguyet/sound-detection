cefc75c68190e669d8dbc2578db98acf6c988710
jguyet <gygydu19@hotmail.fr>
2023-07-31 04:10:25 +0200

    add voice detection

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ var pcm = require('pcm-boilerplate');
 var qmean = require('compute-qmean');
 var mean = require('compute-incrmmean');
 
+function bufjoin(bufs){
+    let aout = new Float32Array(bufs[0].length + bufs[1].length);
+    aout.set(bufs[0], 0);
+    aout.set(bufs[1], bufs[0].length);
+    return aout;
+}
+
 function NoiseDetection(options, callback) {
     var streamDecoder = new pcm.StreamDecoder(options.format);
     var rmsAvg = mean(2000);
+    var record = undefined;
 
     function toDecibel(rms) {
-        return 20*(Math.log(rms) / Math.log(10));
+        return rms*1000;
     }
 
     function processBlock() {
@@ -24,12 +32,36 @@ function NoiseDetection(options, callback) {
             rms = qmean(samples);
             rmsAvg(rms);
             dB = toDecibel(rms);
-            if (options.triggerLevel) {
-                if (dB > toDecibel(rmsAvg()) + options.triggerLevel) {
-                    callback(dB);
+            const pieceSoundAverage = toDecibel(rmsAvg());
+            if (dB > pieceSoundAverage + options.triggerLevel) {
+                if (record == undefined) {
+                    record = {
+                        startAvg: rmsAvg(rms) * 1.10,
+                        currentAvg: mean(100)(rms),
+                        avg: mean(100),
+                        start: true,
+                        end: false,
+                        blocks: block[0]
+                    };
+                } else {
+                    record.avg(rms);
+                    record.currentAvg = record.avg(rms);
+                    record.blocks = bufjoin([record.blocks, block[0]]);
                 }
+                callback(dB, record);
             } else {
-                callback(dB);
+                if (record != undefined) {
+                    record.avg(rms);
+                    record.currentAvg = record.avg(rms);
+                    record.blocks = bufjoin([record.blocks, block[0]]);
+                    if (record.avg() < record.startAvg) {
+                        record.end = true;
+                        callback(dB, { ... record });
+                        record = undefined;
+                    } else {
+                        callback(dB, record);
+                    }
+                }
             }
         }
     }
